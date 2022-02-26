8f61fe03eab347b379fbcec5ff0ebb0a61b7f119
salvatoreromeo <info@salvatoreromeo.com>
2022-02-26 16:39:28 +0100

    add devtool v0

---
+++ packages/httply-frontend/src/components/devtool-request.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+    import {inputStore} from "../stores/input.store";
+
+</script>
+
+{#if $inputStore.request}
+    <div class="hl-devtool-request">
+        url: {$inputStore.request.url}
+        method: {$inputStore.request.options?.method}
+    </div>
+{/if}
+
+
+<style lang="scss">
+
+  .hl-devtool-request {
+
+  }
+
+</style>
---
+++ packages/httply-frontend/src/stores/devtool.store.ts
@@ -0,0 +1,18 @@
+import {createStore, loggedWritable} from "../shared/store.util";
+
+export interface DevtoolStore {
+    tabs: string[],
+    activeTab:string
+}
+
+
+export const devtoolStore = loggedWritable<DevtoolStore>({
+    tabs: ["Request", "Headers", "Payload", "Response", "Cookies"],
+    activeTab: "Request"
+})
+
+
+
+export function updateActiveTab(tab:string){
+    devtoolStore.update({activeTab: tab})
+}
