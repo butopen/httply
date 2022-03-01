863aa2e5850f901f623c41c637a5f5ac64bed84d
salvatoreromeo <info@salvatoreromeo.com>
2022-03-01 16:32:31 +0100

    add JSON viewer svelte component and Json type

---
+++ packages/httply-frontend/src/components/json-viewer/json-viewer.svelte
@@ -0,0 +1,133 @@
+<script lang="ts">
+
+    import type {Json} from "../../shared/json.model";
+    import {JsonViewerLogic, RenderedJsonRow} from "./json-viewer.logic";
+
+    export let json: Json = {}
+    export let isMaster: boolean = true
+
+    const jsonViewer = new JsonViewerLogic()
+
+    let rendered: RenderedJsonRow[] = []
+
+    $: {
+        rendered = jsonViewer.toRenderedValues(json)
+    }
+
+    function onToggle(e: MouseEvent, r: RenderedJsonRow) {
+        e.stopPropagation()
+        if (['object', 'array'].includes(r.jsonType)) {
+            r.expanded = !r.expanded
+        }
+        rendered = [...rendered]
+    }
+
+</script>
+<div class="hl-json-viewer">
+    <ul class:master={isMaster}>
+        {#each rendered as r}
+            <li class:expanded={r.expanded}>
+                <div on:click={e => onToggle(e, r)} class="json-viewer-row {r.jsonType}">
+                    <span class="json-viewer-key">{r.key}</span>
+                    <span class="json-viewer-ddots">:</span>
+                    {#if !r.expanded}
+                        <span title={r.tooltip} class="json-viewer-value collapsed {r.jsonType}">{r.value}</span>
+                    {/if}
+                </div>
+                {#if r.expanded}
+                    <svelte:self isMaster={false} json={r.json} parent={r.key}/>
+                {/if}
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style lang="scss">
+
+  .hl-json-viewer {
+    @apply block bg-white
+  }
+
+  ul {
+    @apply p-0 m-0 list-none ml-2.5
+  }
+
+  ul.master {
+    @apply pt-1
+  }
+
+  ul:not(.master) {
+    @apply border-l border-gray-100
+  }
+
+  ul:not(.master):hover {
+    @apply border-gray-200
+  }
+
+
+  li {
+    @apply p-0 m-0 list-none ml-3 text-xs leading-4;
+    width: 600px;
+  }
+
+  .json-viewer-row {
+    @apply inline-flex leading-4 h-4
+  }
+
+  .json-viewer-key {
+    color: #a000a5;
+    @apply relative
+  }
+
+  .json-viewer-ddots {
+    color: #240024;
+  }
+
+  .json-viewer-value {
+    @apply inline-block ml-2
+  }
+
+  .json-viewer-value.string {
+    color: #008b00;
+  }
+
+  .json-viewer-value.number {
+    color: #5e37ff;
+  }
+
+  .json-viewer-value.null {
+    color: #ec4a00;
+  }
+
+  .json-viewer-value.object, .json-viewer-value.array {
+    @apply w-full truncate
+  }
+
+  .json-viewer-value.string:before, .json-viewer-value.string:after {
+    content: '"';
+  }
+
+  .json-viewer-row:before {
+    content: '';
+    @apply m-0.5 w-4 h-4 inline-block
+  }
+
+  .json-viewer-row.object, .json-viewer-row.array {
+    @apply cursor-pointer
+  }
+
+  .json-viewer-row.object:before, .json-viewer-row.array:before {
+    content: url("data:image/svg+xml; utf8, <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 16 16\"><path d=\"M10,4 L14,7 L10,10 z\"/></svg>");
+    opacity: 50%;
+    transition: transform 150ms ease-in-out;
+  }
+
+  .json-viewer-value.object.collapsed {
+    @apply whitespace-nowrap text-gray-400 truncate max-w-sm
+  }
+
+  li.expanded > .json-viewer-row.object:before, li.expanded > .json-viewer-row.array:before {
+    transform: rotate(90deg);
+  }
+
+</style>
---
+++ packages/httply-frontend/src/shared/json.model.ts
@@ -0,0 +1,7 @@
+
+
+export type Json = void | Date | null | boolean | number | string | Json[] | { [prop: string]: Json }
+
+export function cloneJson(obj) {
+    return JSON.parse(JSON.stringify(obj))
+}
