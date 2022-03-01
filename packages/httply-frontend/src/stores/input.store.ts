6aa3021bda9a96e09119adbd791122e496985a79
salvatoreromeo <info@salvatoreromeo.com>
2022-03-01 16:45:34 +0100

    refactoring of the stores

--- packages/httply-frontend/src/stores/input.store.ts
+++ packages/httply-frontend/src/stores/input.store.ts
@@ -1,36 +1,22 @@
 import {loggedWritable} from "../shared/store.util";
 import {fetchParser} from "../shared/fetch-parser";
 import type {HttplyInput} from "../shared/httply.model";
-import {viewStore} from "./view.store";
+import {updateHttpRequest} from "./view.store";
 
 export const inputStore = loggedWritable<HttplyInput>({
     httpInput: ""
 })
 
-export function updateHttpDomain(domain: string) {
-    viewStore.update(s => {
-        s.request.general.data.Domain = domain
-        return s
-    })
-}
-export function updateHttpReferrer(referrer: string) {
-    viewStore.update(s => {
-        s.request.general.data.Referrer = referrer
-        return s
-    })
-}
 
 export function updateHttpInput(httpInput: string) {
     const request = fetchParser(httpInput)
-    viewStore.update(s => {
-        s.request.general.data = {"Request URL": request.url, "Method": request.options.method}
-        return s
-    })
-    try{
-        if(request.options.referrer){
-            updateHttpDomain(request.options.referrer)
-            updateHttpReferrer(request.options.referrer)
+    updateHttpRequest("Url", request.url)
+    try {
+        if (request.options.referrer) {
+            updateHttpRequest("Referrer", request.options.referrer)
+            updateHttpRequest("Domain", request.options.referrer)
         }
-    } catch (e){}
+    } catch (e) {
+    }
     inputStore.update({httpInput, request})
 }
