6c4111b14e789354df91603b45edd5e687ba5462
salvatoreromeo <info@salvatoreromeo.com>
2022-03-08 12:14:47 +0100

    rename httply proxy and add httply backend

--- packages/httply-proxy/src/httply-backend.model.ts
+++ packages/httply-proxy/src/httply-backend.model.ts
@@ -1,15 +1,15 @@
 export interface HLRequestInformation {
-    url: string;
-    options: RequestInit;
+  url: string;
+  options: RequestInit;
 }
 
 export interface HLRequest extends Request {
-    content: HLRequestInformation;
+  content: HLRequestInformation;
 }
 
 export interface HLResponseInformation {
-    request: HLRequestInformation;
-    time: number;
-    status: number;
-    headers: { [h: string]: string };
+  request: HLRequestInformation;
+  time: number;
+  status: number;
+  headers: { [h: string]: string };
 }
--- packages/httply-proxy/src/index.ts
+++ packages/httply-proxy/src/index.ts
@@ -1,13 +1,13 @@
-import {router} from "./router";
+import { router } from "./router";
 
 export default {
-    async fetch(request, environment, context) {
-        try {
-            const response = await router.handle(request);
-            console.log("response: ", response);
-            return response;
-        } catch (error) {
-            return new Response("Oops!", {status: 500});
-        }
+  async fetch(request, environment, context) {
+    try {
+      const response = await router.handle(request);
+      console.log("response: ", response);
+      return response;
+    } catch (error) {
+      return new Response("Oops!", { status: 500 });
     }
+  }
 };
