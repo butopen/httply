551fad6ff9b8f120a2540b8c7e16d48c17f9e7ab
salvatoreromeo <info@salvatoreromeo.com>
2022-03-09 18:02:00 +0100

    fix local env

--- packages/httply-frontend/src/apis/http.api.ts
+++ packages/httply-frontend/src/apis/http.api.ts
@@ -1,6 +1,6 @@
 import type { Json } from '../shared/json.model';
 
-const basePath = window.location.href.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'https://api.httply.com/be';
+const basePath = window.location.href.indexOf('http://localhost:3000') == 0 ? 'http://localhost:3001' : 'https://api.httply.com/be';
 
 export async function post(path: string, body: any) {
   const url = path.startsWith('http') ? path : basePath + path;
