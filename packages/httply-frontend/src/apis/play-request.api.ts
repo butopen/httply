5e3f1cd47bccf402b936696ac39d136cd040874f
salvatoreromeo <info@salvatoreromeo.com>
2022-03-09 10:56:58 +0100

    always use httply api

--- packages/httply-frontend/src/apis/play-request.api.ts
+++ packages/httply-frontend/src/apis/play-request.api.ts
@@ -1,24 +1,13 @@
 export async function playRequest(url: string, options) {
   let result = { headers: {}, body: '' };
-  try {
-    const resp = await fetch(url, options);
-    let text = await resp.text();
-    result.body = text;
-    try {
-      const json = JSON.parse(text);
-      result.body = json;
-    } catch (e) {}
-    resp.headers.forEach((hv, h) => {
-      result.headers[h] = hv;
-    });
-  } catch (e) {
-    const resp = await fetch('https://httply.com/api/', {
-      method: 'POST',
-      body: JSON.stringify({ url, options })
-    });
-    const json = await resp.json();
-    result.headers = json.response.headers;
-    result.body = json.response.response;
-  }
+
+  const resp = await fetch('https://httply.com/api/', {
+    method: 'POST',
+    body: JSON.stringify({ url, options })
+  });
+  const json = await resp.json();
+  result.headers = json.response.headers;
+  result.body = json.response.response;
+
   return result;
 }
