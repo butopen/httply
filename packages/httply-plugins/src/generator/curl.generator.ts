49ac92de64707cd3f30aa58c412220dbe51fc839
salvatoreromeo <info@salvatoreromeo.com>
2022-03-25 16:03:44 +0100

    parser brainstorming

--- packages/httply-plugins/src/generator/curl.generator.ts
+++ packages/httply-plugins/src/generator/curl.generator.ts
@@ -1,43 +1,56 @@
 import { HttplyRequest } from "@butopen/httply-model";
-import {HttplyGenerator} from "./generator";
+import { HttplyGenerator } from "./generator";
 
 export class CurlGenerator implements HttplyGenerator {
+  paramDelimiter: "'" | '"' = "'";
+  quoteDelimiter: "" | "^\\^" = "";
 
-    /**
-     * given an HttplyRequest object, parse it and return curl command
-     * @param: request
-     * */
-    generate(request: HttplyRequest): string {
-        /*another possible param is a boolean selector for choose between cmd and bash curl syntax*/
+  constructor(private options: { target: "cmd" | "bash" } = { target: "cmd" }) {
+    if (options.target == "cmd") {
+      this.quoteDelimiter = "^\\^";
+      this.paramDelimiter = '"';
+    }
+  }
 
-        /** command will be formatted with the following syntax
-         *   curl [-X ...] url [optional header and data!]
-         */
-        let curlCommand = [`curl`]; //always begin with curl keyword
+  /**
+   * given an HttplyRequest object, parse it and return curl command
+   * @param: request
+   * */
+  generate(request: HttplyRequest): string {
+    /*another possible param is a boolean selector for choose between cmd and bash curl syntax*/
 
-        // handle HTTP method
-        if (request.options.method != 'GET') {
-            curlCommand.push(`-X ${request.options.method}`);
-        }
+    /** command will be formatted with the following syntax
+     *   curl [-X ...] url [optional header and data!]
+     */
+    let curlCommand = [`curl`]; //always begin with curl keyword
 
-        // insert URL
-        curlCommand.push(`${request.url}`);
-
-        //handle parameters
-        if(request.options.headers) {
-            for (let headerKey in request.options.headers) {
-                let headerValue = request.options.headers[headerKey];
-                curlCommand.push(`-H '${headerKey}:${headerValue}'`)
-            }
-        }
-
-        //handle data
-        if(request.body){
-            curlCommand.push(`-d ${request.body}`)
-        }
-
-        return curlCommand.join(" ");
+    // handle HTTP method
+    if (request.options.method != "GET") {
+      curlCommand.push(`-X ${request.options.method}`);
     }
 
+    // insert URL
+    curlCommand.push(`${request.url}`);
 
+    //handle parameters
+    if (request.options.headers) {
+      for (let headerKey in request.options.headers) {
+        let rawHeaderValue = request.options.headers[headerKey];
+        let headerValue =
+          this.options.target == "bash"
+            ? rawHeaderValue
+            : rawHeaderValue.replace(/"/g, this.quoteDelimiter + '"');
+        curlCommand.push(
+          `-H ${this.paramDelimiter}${headerKey}:${headerValue}${this.paramDelimiter}`
+        );
+      }
+    }
+
+    //handle data
+    if (request.body) {
+      curlCommand.push(`-d ${request.body}`);
+    }
+
+    return curlCommand.join(" ");
+  }
 }
--- packages/httply-plugins/src/parser/curl.parser.ts
+++ packages/httply-plugins/src/parser/curl.parser.ts
@@ -4,137 +4,137 @@ import { HttplyMethod } from "@butopen/httply-model";
 import * as http from "http";
 
 export class CurlParser implements HttplyParser {
-    private regexExpression: RegExp = /[\s|']-/;
+  private regexExpression: RegExp = /\s+/;
 
-    parse(request: string): HttplyRequest {
-        if (this.canApply(request)) {
-            let parsedRequest = {
-                options: {headers: {}, method: "GET"} as HttplyRequest["options"],
-                url: "",
-            } as HttplyRequest;
+  parse(request: string): HttplyRequest {
+    if (this.canApply(request)) {
+      let parsedRequest = {
+        options: { headers: {}, method: "GET" } as HttplyRequest["options"],
+        url: "",
+      } as HttplyRequest;
 
-            const normalizedRequest = this.preFilterRequest(request);
-            if (this.hasParams(normalizedRequest)) {
-                let params = normalizedRequest.split(this.regexExpression);
-                // console.log(params)
-                params.forEach((element) => {
-                    this.handleParam(element,parsedRequest) ;
-                });
-            } else {
-                parsedRequest.url = normalizedRequest.split(" ")[1] || "";
-            }
-            parsedRequest.timestamp = new Date().getTime();
-            console.log(parsedRequest)
-            return parsedRequest;
+      const normalizedRequest = this.preFilterRequest(request);
+      if (this.hasParams(normalizedRequest)) {
+        let params = normalizedRequest.split(this.regexExpression);
+        // console.log(params)
+        params.forEach((element) => {
+          this.handleParam(element, parsedRequest);
+        });
+      } else {
+        parsedRequest.url = normalizedRequest.split(" ")[1] || "";
+      }
+      parsedRequest.timestamp = new Date().getTime();
+      console.log(parsedRequest);
+      return parsedRequest;
+    } else {
+      throw new ParseError();
+    }
+  }
+
+  /**
+   * Check if param request starts with curl keyword
+   * @param request
+   */
+  canApply(request: string): boolean {
+    return request.trim().startsWith("curl");
+  }
+
+  /**
+   * Check if there is some -Z -X -H -D params or we have an extended version
+   * @param request
+   */
+  hasParams(request: string): boolean {
+    return request.split(this.regexExpression).length != 1;
+  }
+
+  /**
+   * cleaning request from escape's keywords
+   * @param request
+   */
+  preFilterRequest(request: string): string {
+    let normalizedRequest: string;
+    if (!request.includes("^")) {
+      //Bash
+      normalizedRequest = request
+        .trim()
+        .replace(/[\r]/g, "")
+        .replace(/[\n]/g, "")
+        .replace(/[\\]/g, "");
+    } else {
+      //cmd
+      normalizedRequest = request
+        .trim()
+        .replace(/[\r]/g, "")
+        .replace(/[\n]/g, "")
+        .replace(/[\\]/g, "")
+        .replace(/[\^]/g, "");
+    }
+
+    return normalizedRequest;
+  }
+
+  handleParam(param: string, httpRequest: HttplyRequest): void {
+    let url = "";
+    let words = param.split(" ");
+    switch (param.trim().split(" ")[0]) {
+      case "X":
+      case "-request": {
+        httpRequest.options.method = words[1] as HttplyMethod;
+        httpRequest.url = words[2];
+        break;
+      }
+      case "-header":
+      case "H": {
+        const [headerKey, ...headerValues] = param
+          .replace(words[0] + " ", "")
+          .split(":");
+        let headerValue = headerValues.join(":");
+
+        httpRequest.options.headers![headerKey] = headerValue.trim();
+        break;
+      }
+      case "d":
+      case "-data-raw":
+      case "-data": {
+        // has d flag and no X param so it's a POST
+        httpRequest.options.method = "POST";
+        let data = {};
+        param = param.replace(words[0] + " ", "");
+
+        if (
+          httpRequest.options.headers!["Content-Type"] != "application/json"
+        ) {
+          let aux = param.split("&");
+          if (aux.length != 1) {
+            aux.forEach((element) => {
+              let el = element.replace(/["|']/g, "").split("=");
+              data[el[0]] = el[1].trim();
+            });
+          } else {
+            data["data"] = param;
+          }
         } else {
-            throw new ParseError();
+          //parse the JSON string
+          let aux = param.replace(words[0] + " ", "");
+          data = JSON.parse(aux);
         }
-    }
-
-    /**
-     * Check if param request starts with curl keyword
-     * @param request
-     */
-    canApply(request: string): boolean {
-        return request.trim().startsWith("curl");
-    }
-
-    /**
-     * Check if there is some -Z -X -H -D params or we have an extended version
-     * @param request
-     */
-    hasParams(request: string): boolean {
-        return request.split(this.regexExpression).length != 1;
-    }
-
-    /**
-     * cleaning request from escape's keywords
-     * @param request
-     */
-    preFilterRequest(request: string): string {
-        let normalizedRequest: string;
-        if (!request.includes("^")) {
-            //Bash
-            normalizedRequest = request
-                .trim()
-                .replace(/[\r]/g, "")
-                .replace(/[\n]/g, "")
-                .replace(/[\\]/g, "")
-                .replace(/[']/g, "");
-        }else {
-            //cmd
-            normalizedRequest = request
-                .trim()
-                .replace(/[\r]/g, "")
-                .replace(/[\n]/g, "")
-                .replace(/[\\]/g, "")
-                .replace(/[\^]/g, "")
-                .replace(/["]/g, "");
+        httpRequest.body = JSON.stringify(data);
+        break;
+      }
+      default: {
+        if (param.includes("curl") && param.includes("http")) {
+          url = param.split(" ")[1];
+          httpRequest.url = url;
         }
-
-        return normalizedRequest;
-    }
-
-    handleParam(param: string, httpRequest:HttplyRequest): void {
-
-        let url = "";
-        let words = param.split(" ");
-        switch (param.trim().split(" ")[0]) {
-            case "X":
-            case "-request": {
-                httpRequest.options.method = words[1] as HttplyMethod;
-                httpRequest.url = words[2];
-                break;
-            }
-            case "-header":
-            case "H": {
-                const [headerKey,...headerValues] = param
-                    .replace(words[0] + " ", "")
-                    .split(":");
-                let headerValue = headerValues.join(":")
-
-                httpRequest.options.headers![headerKey] = headerValue.trim();
-                break;
-            }
-            case "d":
-            case "-data-raw":
-            case "-data": {
-                // has d flag and no X param so it's a POST
-                httpRequest.options.method = "POST";
-                let data = {};
-                param = param
-                    .replace(words[0]+" ","");
-
-
-                if (httpRequest.options.headers!['Content-Type'] != "application/json"){
-                    let aux = param.split("&");
-                    if(aux.length!=1) {
-                        aux.forEach((element) => {
-                            let el = element.replace(/["|']/g, "").split("=");
-                            data[el[0]] = el[1].trim();
-                        });
-                    }else{
-                        data["data"] = param;
-                    }
-                } else {
-                    //parse the JSON string
-                    let aux = param.replace(words[0] + " ", "");
-                    data = JSON.parse(aux);
-                }
-                httpRequest.body = JSON.stringify(data);
-                break;
-            }
-            default: {
-                if (param.includes("curl") && param.includes("http")) {
-                    url = param.split(" ")[1];
-                    httpRequest.url  = url;
-                }
-                if (!(httpRequest.body == undefined) && (httpRequest.body.includes('"-data-raw"}'))){
-                    //the following param string overwrite body content
-                    httpRequest.body = param;
-                }
-                break;
-            }
+        if (
+          !(httpRequest.body == undefined) &&
+          httpRequest.body.includes('"-data-raw"}')
+        ) {
+          //the following param string overwrite body content
+          httpRequest.body = param;
         }
+        break;
+      }
     }
+  }
 }
