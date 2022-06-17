dc9e8b104c4639d041460e121bbf857db5585fa9
Emanuele <emanuele.antimiani@gmail.com>
2022-06-17 15:16:45 +0200

    deleted instrumentation

--- packages/httply-backend/src/requests/requests.functions.ts
+++ packages/httply-backend/src/requests/requests.functions.ts
@@ -1,11 +1,3 @@
-import {instrument_requestFromMeta,useInstrumented_requestFromMeta} from './instrumentation/requests.functions'
-/* decorated by notest... just ignore -> */if(useInstrumented_requestFromMeta()){(requestFromMeta as any) =  instrument_requestFromMeta()}
-import {instrument_requestMeta,useInstrumented_requestMeta} from './instrumentation/requests.functions'
-/* decorated by notest... just ignore -> */if(useInstrumented_requestMeta()){(requestMeta as any) =  instrument_requestMeta()}
-import {instrument_responseHash,useInstrumented_responseHash} from './instrumentation/requests.functions'
-/* decorated by notest... just ignore -> */if(useInstrumented_responseHash()){(responseHash as any) =  instrument_responseHash()}
-import {instrument_requestHash,useInstrumented_requestHash} from './instrumentation/requests.functions'
-/* decorated by notest... just ignore -> */if(useInstrumented_requestHash()){(requestHash as any) =  instrument_requestHash()}
 import { HttplyRequest, HttplyResponse } from '@butopen/httply-model';
 import { CryptService } from '../shared/crypt/crypt.service';
 
