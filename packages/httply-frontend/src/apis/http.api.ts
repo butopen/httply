7100e93477064c2b9d8c76ab788325d65db760b2
salvatoreromeo <info@salvatoreromeo.com>
2022-06-21 17:20:02 +0200

    fix pnpm + update API

--- packages/httply-frontend/src/apis/http.api.ts
+++ packages/httply-frontend/src/apis/http.api.ts
@@ -1,6 +1,9 @@
 import type { Json } from '../shared/json.model';
 
-const basePath = window.location.href.indexOf('http://localhost:3000') == 0 ? 'http://localhost:3001' : 'https://api.httply.com';
+const basePath =
+  window.location.href.indexOf('http://localhost:3000') == 0
+    ? 'http://localhost:3001'
+    : 'https://v2.api.httply.com';
 
 export async function post(path: string, body: any) {
   const url = path.startsWith('http') ? path : basePath + path;
